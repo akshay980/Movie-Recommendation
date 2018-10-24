bfe7b1c860552c052690130669e944c990f99878
akshay980 <33752064+akshay980@users.noreply.github.com>
2018-10-24 23:48:50 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,43 @@
+
+# Collabrative Filtering data set.
+
+
+dataset={
+			'Akshay': {'Raees': 2.5, 
+							'3 idiots': 3.5,
+							'Sanju': 3.0, 
+							'Padmaavat': 3.5, 
+							'Bahubali': 2.5,
+							'Gold': 3.0},
+              
+			'Kashyap': {'Raees': 3.0, 
+							'3 idiots': 3.5,
+							'Sanju': 1.5,
+							 'Padmaavat': 5.0, 
+							 'Gold': 3.0,
+							'Bahubali': 3.5},
+
+			'Parth': {'Raees': 2.5, 
+								'3 idiots': 3.0,
+								'Padmaavat': 3.5,
+								 'Gold': 4.0},
+			'Smit': {'3 idiots': 3.5, 
+							'Sanju': 3.0,
+							'Gold': 4.5, 
+							'Padmaavat': 4.0,
+							'Bahubali': 2.5},
+			'Jeet': {'Raees': 3.0, 
+							'3 idiots': 4.0,
+							'Sanju': 2.0, 
+							'Padmaavat': 3.0, 
+							'Gold': 3.0,
+							'Bahubali': 2.0},
+			'Harsh': {'Raees': 3.0, 
+							'3 idiots': 4.0,
+							'Gold': 3.0, 
+							'Padmaavat': 5.0, 
+							'Bahubali': 3.5},
+			'Madhav': {'3 idiots':4.5,
+					'Bahubali':1.0,
+					'Padmaavat':4.0}
+            }
